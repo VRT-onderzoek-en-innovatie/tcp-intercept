201f885fdac5c45d078e781594485950ffac1866
Niels Laukens <niels.laukens@vrt.be>
2013-04-15 11:09:49 +0200

    Added connection from&to output

--- src/tcp-intercept.cxx
+++ src/tcp-intercept.cxx
@@ -2,6 +2,8 @@
 #include "../Socket/Socket.hxx"
 #include <netinet/in.h>
 
+const static unsigned short listen_port = 5000;
+
 int main(int argc, char* argv[]) {
 	Socket s = Socket::socket(AF_INET, SOCK_STREAM, 0);
 
@@ -11,12 +13,19 @@ int main(int argc, char* argv[]) {
 #endif
 
 	std::auto_ptr<SockAddr::SockAddr> bind_addr(
-		SockAddr::translate("0.0.0.0", 5000) );
+		SockAddr::translate("0.0.0.0", listen_port) );
 
 	s.bind(*bind_addr);
-
 	s.listen(10);
-	Socket con = s.accept(NULL);
+	std::cout << "Listening on " << bind_addr->string() << std::endl;
+
+	std::auto_ptr<SockAddr::SockAddr> client_addr;
+	Socket con = s.accept(&client_addr);
+	std::cout << "Connection established from " << client_addr->string() << std::endl;
+
+	std::auto_ptr<SockAddr::SockAddr> server_addr;
+	server_addr = con.getsockname();
+	std::cout << "Connection established to   " << server_addr->string() << std::endl;
 
 	con.send("Hello world\n");
 	std::cout << "Received:\n" << con.recv();
