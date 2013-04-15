d1a4f101464a14c9bc6be5e6a3b0e6f55c611d9c
Niels Laukens <niels.laukens@vrt.be>
2013-04-15 10:46:45 +0200

    Made IP_TRANSPARENT optional (and thus this build useless) to develop on non-linux platforms

--- src/tcp-intercept.cxx
+++ src/tcp-intercept.cxx
@@ -5,8 +5,10 @@
 int main(int argc, char* argv[]) {
 	Socket s = Socket::socket(AF_INET, SOCK_STREAM, 0);
 
+#if HAVE_DECL_IPPROTO_IP && HAVE_DECL_IP_TRANSPARENT
 	int value = 1;
-	s.setsockopt(IPPROTO_IP, IP_TRANSPARENT, &value, sizeof(value)); // TODO: IPv6
+	s.setsockopt(IPPROTO_IP, IP_TRANSPARENT, &value, sizeof(value)); // TODO: IPPROTO_IPV6
+#endif
 
 	std::auto_ptr<SockAddr::SockAddr> bind_addr(
 		SockAddr::translate("0.0.0.0", 5000) );
